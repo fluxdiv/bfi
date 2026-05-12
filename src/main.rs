b2db88fe4e3d597850a444538d00aa7e430ce199
fluxdiv <156196590+fluxdiv@users.noreply.github.com>
2026-05-12 15:29:34 -0400

    feat: add multithreading, minheap, handler for permissions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 // use std::path::PathBuf;
 use std::process::Command;
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
 use anyhow::{Result, anyhow};
 use quickfig::derive::ConfigFields;
 use quickfig::core::{
@@ -27,7 +31,7 @@ struct Include {
     access: Option<Vec<String>>
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum CmdOutput {
     GeneralPath(String),
     GeneralType(String),
@@ -51,47 +55,123 @@ enum CmdOutput {
     AccessBirth(String)
 }
 
-
-
 // shelling out to other tools here because
 // 1) they work
 // 2) they're mostly ubiquitous, with some missing cases handled
 // 3) there isn't a benefit to the syscall route for a tool like this
 impl Include {
-    fn handle_general(&self) {
+    fn handle_general(
+        &self,
+        heap: Arc<Mutex<BinaryHeap<Reverse<CmdOutput>>>>
+    ) -> Vec<JoinHandle<()>> {
         if let Some(generals) = &self.general {
-            generals.iter().for_each(|o| {
+            let threads: Vec<JoinHandle<()>> = generals.iter().map(|o| {
                 match o.as_str() {
                     "path" => {
-                        // readlink -f filename
-                        let out = Command::new("readlink")
-                            .arg("-f")
-                            .arg(TFILE)
-                            .output()
-                            .unwrap();
+                        let heap = Arc::clone(&heap);
+                        thread::spawn(move || {
+                            // readlink -f filename
+                            let out = Command::new("readlink")
+                                .arg("-f")
+                                .arg(TFILE)
+                                .output()
+                                .unwrap();
 
-                        let b = String::from_utf8(out.stdout).unwrap();
+                            let b = String::from_utf8(out.stdout).unwrap();
+                            let cmdout = CmdOutput::GeneralPath(b.trim().to_string());
 
-                        println!("---------");
-                        println!("path output");
-                        println!("{b}");
+                            let mut heaplock = heap.lock().unwrap();
+                            heaplock.push(Reverse(cmdout));
+                        })
                     },
                     "type" => {
-                        // file file.name
-                        let out = Command::new("file")
-                            .arg(TFILE)
-                            .output()
-                            .unwrap();
-                        let b = String::from_utf8(out.stdout).unwrap();
-                        println!("---------");
-                        println!("file output");
-                        println!("{b}");
+                        let heap = Arc::clone(&heap);
+                        thread::spawn(move || {
+                            // file file.name
+                            let out = Command::new("file")
+                                .arg(TFILE)
+                                .output()
+                                .unwrap();
+                            let b = String::from_utf8(out.stdout).unwrap();
+                            let cmdout = CmdOutput::GeneralType(b.trim().to_string());
+
+                            let mut heaplock = heap.lock().unwrap();
+                            heaplock.push(Reverse(cmdout));
+                        })
                     },
-                    _ => {/* ignore for now */}
+                    _ => todo!()
                 }
-            });
+            }).collect();
+            return threads;
         }
+        vec![]
     }
+
+    fn handle_permissions(
+        &self,
+        heap: Arc<Mutex<BinaryHeap<Reverse<CmdOutput>>>>
+    ) -> Vec<JoinHandle<()>> {
+        let mut ret: Vec<JoinHandle<()>> = vec![];
+        if let Some(permissions) = &self.permissions {
+            // get all regardless via 1 command/thread,
+            // but only keep what the user asked for
+            if permissions.len() > 0 {
+                let heap = Arc::clone(&heap);
+                let permissions = permissions.clone();
+                ret.push(thread::spawn(move || {
+                    let out = Command::new("stat")
+                        .arg("-c")
+                        .arg("'%A|%U %u|%G %g'")
+                        .arg(TFILE)
+                        .output()
+                        .unwrap();
+                    let b = String::from_utf8(out.stdout).unwrap();
+                    let outs: Vec<&str> = b
+                        .trim()
+                        .trim_matches('\'')
+                        .split('|')
+                        .collect();
+                    println!("{:#?}", outs);
+
+                    permissions.iter().for_each(|p| {
+                        match p.as_str() {
+                            "permissions" => {
+                                let out = outs
+                                    .get(0)
+                                    .unwrap_or_else(|| &"")
+                                    .to_string();
+                                let cmdout = CmdOutput::PermissionsPermissions(out);
+                                let mut heaplock = heap.lock().unwrap();
+                                heaplock.push(Reverse(cmdout));
+                            },
+                            "owner" => {
+                                let out = outs
+                                    .get(1)
+                                    .unwrap_or_else(|| &"")
+                                    .to_string();
+                                let cmdout = CmdOutput::PermissionsOwner(out);
+                                let mut heaplock = heap.lock().unwrap();
+                                heaplock.push(Reverse(cmdout));
+                            },
+                            "group" => {
+                                let out = outs
+                                    .get(2)
+                                    .unwrap_or_else(|| &"")
+                                    .to_string();
+                                let cmdout = CmdOutput::PermissionsGroup(out);
+                                let mut heaplock = heap.lock().unwrap();
+                                heaplock.push(Reverse(cmdout));
+                            },
+                            _ => todo!()
+                        }
+                    });
+                }));
+            }
+        }
+        ret
+    }
+
+
 }
 
 fn main() -> Result<()> {
@@ -116,8 +196,22 @@ fn main() -> Result<()> {
 
     let include: Include = Include::deserialize(include_inner)?;
 
-    include.handle_general();
+    let mut heap = Arc::new(Mutex::new(BinaryHeap::<Reverse<CmdOutput>>::new()));
 
+    let mut threads: Vec<JoinHandle<()>> = vec![];
+
+    threads
+        .extend(include.handle_general(Arc::clone(&heap)));
+    threads
+        .extend(include.handle_permissions(Arc::clone(&heap)));
+
+    threads.into_iter().for_each(|t| t.join().unwrap());
+
+    let mut heap = heap.lock().unwrap();
+
+    while let Some(v) = heap.pop() {
+        println!("{:?}", v.0);
+    }
 
     Ok(())
 }
